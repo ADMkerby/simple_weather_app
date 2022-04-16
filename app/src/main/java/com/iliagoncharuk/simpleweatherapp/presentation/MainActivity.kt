f0a0f8e0d24ac71a7554605c8546a50b787d0cae
ADMkerby <goncharuk.1997.man@gmail.com>
2022-04-16 21:31:04 +0500

    Added progress bar

--- app/src/main/java/com/iliagoncharuk/simpleweatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/iliagoncharuk/simpleweatherapp/presentation/MainActivity.kt
@@ -9,10 +9,7 @@ import android.os.Looper
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,6 +17,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.*
@@ -174,11 +172,16 @@ fun MainView(viewModel: WeatherViewModel, permissionListener:PermissionListener)
                 Column(modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text("Weather in", style = MaterialTheme.typography.h6)
-                    viewModel.state.value.weather?.let { Text(it.cityName, style = MaterialTheme.typography.h6) }
-                    Spacer(modifier = Modifier.height(20.dp))
-                    viewModel.state.value.weather?.weatherDescription?.get(0)?.let { Text(it.main) }
-                    Text("${viewModel.state.value.weather?.metricData?.temp}°")
+                    if (viewModel.state.value.weather != null){
+                        Text("Weather in", style = MaterialTheme.typography.h6)
+                        viewModel.state.value.weather?.let { Text(it.cityName, style = MaterialTheme.typography.h6) }
+                        Spacer(modifier = Modifier.height(20.dp))
+                        viewModel.state.value.weather?.weatherDescription?.get(0)?.let { Text(it.main) }
+                        Text("${viewModel.state.value.weather?.metricData?.temp}°")
+                    }else{
+                        ShowProgressBar()
+                    }
+
                 }
 
             }
@@ -204,4 +207,21 @@ fun StartButtonView(onContinueClicked: () -> Unit){
 
         }
     }
+}
+
+@Composable
+fun ShowProgressBar(){
+
+    ComposeBasicsTheme() {
+        Surface() {
+            Column(modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                CircularProgressIndicator()
+            }
+
+
+        }
+    }
+
 }
